bed99f2f6002c33365844550aceac7b493fe0bac
Gerrard2022 <kaberukagerard8@gmail.com>
2024-12-05 04:53:14 +0200

    limbo II

---
+++ app/(routes)/success/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const page = () => {
+  return (
+      <div className="bg-white">
+      
+          <div className="px-4 py-16 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-black">YOUR ORDER WAS SUCCESSFUL AND YOU WILL BE CONTACTED SHORTELY</h1>
+            <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+              <div className="sm:col-span-7">
+                  <p className="text-neutral-500">Please watch your email inbox for : Info@igiti.africa or expect a call from shortly after your order from +1 (443) 718-8247</p>  
+                <ul>
+                </ul>
+              </div> 
+            </div>
+          </div>
+      </div>
+  );
+}
+
+export default page
